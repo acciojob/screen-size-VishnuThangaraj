663ec4bc3b5479dd6df197ae3def99c8d7bcd9a7
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-26 21:29:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const height = document.getElementById('height');
+const width = document.getElementById('width');
+
+window.onload = function(){
+	height.innerHTML = window.innerHeight;
+    width.innerHTML = window.innerWidth;
+}
+
+window.onresize = function(){
+	height.innerHTML = window.innerHeight;
+    width.innerHTML = window.innerWidth;
+}
