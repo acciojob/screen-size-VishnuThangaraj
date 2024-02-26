518e192da294e8993f75fc00612ebb7bb596a481
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-26 21:46:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const result = document.getElementById('sizeInfo');
+let result = document.getElementById('sizeInfo');
 
 window.onload = function(){
 	result.innerHTML = `<h1> Width: ${window.innerWidth} and Height: ${window.innerHeight}</h1>`;
